9bf4fbb0fd1421b1c1a6a82081901fb9b19ae092
fish-yan <757094197@qq.com>
2016-01-29 11:59:57 +0800

    Update ViewController.m

--- AliPay/ViewController.m
+++ AliPay/ViewController.m
@@ -21,11 +21,11 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 - (IBAction)pay:(UIButton *)sender {
-    NSString *partner = @"2088501566833063";
+    NSString *partner = @"";
     //收款账户
-    NSString *seller = @"chenglianshiye@yeah.net";
+    NSString *seller = @"";
     //私钥
-    NSString *privateKey = @"MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBALS8ErohdAbxj+vz4DdLksKlWi+s5yd3zCjx712emFQxEduPIm3eL1rlhtYtNEb9JfdevN3uVhWqUlzqEvkb484IRZ1DWK0pvf3vo0gXYDiaMKqxjQqPo21ByPNnvRsrxq6YkP1CoTT6w/MEV8ylaCfc7fHOgZle49Pn98Z2sub9AgMBAAECgYEArJUvOMe7GOpQqVqez4593RqybPYpYRnXPX4ROY+5HCQjTkp28P0KsTyeLiTKV8NiHr47kZ0GXPfgYFMwvOmx9C/pE6DDpNlAaWDEciQXDzBv9jAJRYgPS5qvfOSsY3Lc5lwnN+mECIwmziPW8FJHM7SgGbrd/XZCyaES8nItFU0CQQDgjuTXWUgCwUQL39Kx+qzeAAelGDeB97jyaGpPa7yaCo8UVt8iojmN7s90KdiyN3/4s3LpMcpAPJePUlvaacGnAkEAzgpcV0TaSSjrYeBZ/5xwEmBU9yVheD8TxD969gIM3mrr/WdGTg65Do+cuiANVPSjHUH54wa0+Z10j6wAFpQ+uwJATFPFrP0H4QfYHUEi2KQgBgV0k8U7eM2+64ZaPEyeeq2EHKG6jocdkQTPNujSYyFCOkKkmGb4HAV8bpbL6d1wmQJAJ+vF/HqwuKAfCzXG+km7RTQ5AjHR8tR15f5Our+m8qlQ1CZgbttXa8TTnxR6wM0tlYuk+SHhisPBQ90Vn0pV5QJAar0Ba8WIzh1Wf5YgAG+Is1j+kbaTx9ltA4pDgsUZB+HOVqpK8+9o1N2NlqzaBale+9iZqAskvjyO1IPGzFIDPQ==";
+    NSString *privateKey = @"";
     if (partner.length == 0 || seller.length ==0 || privateKey.length == 0) {
         UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示" message:@"缺少partner或者seller或者私钥。" preferredStyle:(UIAlertControllerStyleAlert)];
         UIAlertAction *action = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:nil];
